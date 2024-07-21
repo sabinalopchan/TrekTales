4636e1ded89fc0c8022dced651792164c91aa479
unknown <sabinalopchan321@gmail.com>
2024-07-21 09:17:48 +0545

    bookmarks updated

--- clients/src/index.js
+++ clients/src/index.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Context, { LoginContext } from './contextProvider/Context';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Context, { LoginContext } from "./contextProvider/Context";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
- <Context>
-
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
- </Context>
-
+  <Context>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Context>
 );
 
 reportWebVitals();
